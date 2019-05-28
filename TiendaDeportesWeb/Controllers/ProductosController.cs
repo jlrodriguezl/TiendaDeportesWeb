09d044daa537580b73b818f7f64bca058ed696e1
SOFTWARE <Rigoberto318722@gmail.com>
2019-05-27 20:41:06 -0500

    avanze funcionalidad consulta

---
+++ TiendaDeportesWeb/Controllers/ProductosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaDeportesWeb.Models;
+using TiendaDeportesWeb.Models.DTOs;
+
+namespace TiendaDeportesWeb.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<ProductoDto> lstProducto = null;
+            using (tiendaEntities db = new tiendaEntities())
+            {
+                lstProducto = (from f in db.PRODUCTOS
+                               orderby f.NOM_PRODUCTO
+                               select new ProductoDto
+                               {
+                                   ID_PRODUCTO = f.ID_PRODUCTO,
+                                   NOM_PRODUCTO = f.NOM_PRODUCTO,
+                                   UND_DISPONIBLES = f.UND_DISPONIBLES
+                               }).ToList();
+            }
+            return View(lstProducto);
+        }
+    }
+}
---
+++ TiendaDeportesWeb/Models/DTOs/ProductoDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiendaDeportesWeb.Models.DTOs
+{
+    public class ProductoDto
+    {
+        public int ID_PRODUCTO { get; set; }
+        public string NOM_PRODUCTO { get; set; }
+        public string DET_PRODUCTO { get; set; }
+        public decimal PRECIO_ACTUAL { get; set; }
+        public int UND_DISPONIBLES { get; set; }
+        public int ID_CATEGORIA { get; set; }
+        public int ID_FABRICANTE { get; set; }
+
+        public virtual CATEGORIAS CATEGORIAS { get; set; }
+        public virtual FABRICANTES FABRICANTES { get; set; }
+    }
+}
