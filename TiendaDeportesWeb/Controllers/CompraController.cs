413bd1c5f9802326293e4865b3a2b8a020acb14a
Alvaro Gutierrez <algut0511@gmail.com>
2019-06-04 20:52:34 -0500

    Avance ventana Ventas

---
+++ TiendaDeportesWeb/Controllers/CompraController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaDeportesWeb.Models.DTOs;
+using TiendaDeportesWeb.DAL;
+namespace TiendaDeportesWeb.Controllers
+{
+    public class CompraController : Controller
+    {
+        public ConsultasGenerales con;
+        // GET: Compra
+        public ActionResult Index()
+        {
+            CompraDTO compra = new CompraDTO();
+            compra.lstFabricantes = con.getListaFabricantes();
+            compra.lstroductos = con.getProductos();
+            compra.lstCategorias = con.GetCategorias();
+            return View();
+        }
+    }
+}
---
+++ TiendaDeportesWeb/Models/DTOs/CompraDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TiendaDeportesWeb.Models.DTOs;
+namespace TiendaDeportesWeb.Models.DTOs
+{
+    public class CompraDTO
+    {
+        //Cliente
+        public PersonasDto cliente { get; set; }
+        //productos disponibles
+        public List <ProductoDto> lstroductos { get; set; }
+
+        //categorias
+        public List<CategoriasDto> lstCategorias { get; set; }
+
+        //marcas
+        public List<FabricantesDTO> lstFabricantes { get; set; }
+    }
+}
